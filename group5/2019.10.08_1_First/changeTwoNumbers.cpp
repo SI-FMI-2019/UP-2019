5a91e10d88428313f9bf4b1e24311fa9f59b133a
Maria <maria.paskova3@gmail.com>
2019-10-08 10:45:33 +0300

    Create changeTwoNumbers.cpp

---
+++ group5/2019.10.08_1_First/changeTwoNumbers.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+
+
+int main()
+{
+    // <type> name;
+    int first;
+    int second;
+    int third;
+
+    cout<<"Write first number"<<endl;
+    cin>>first;
+    cout<<"Write second number"<<endl;
+    cin>>second;
+    cout <<first<<" "<<second<< endl;
+    third = first;
+    first = second;
+    second = third;
+    cout << first<<" "<<second<< endl;
+    return 0;
+}
