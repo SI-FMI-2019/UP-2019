f5b035ad7e983856cb000f572c32003096436e87
elenatuparova <elena.tuparova@gmail.com>
2019-10-21 21:21:48 +0300

    Fix solutions

--- group06/lab02/solutions/task01.cpp
+++ group06/lab02/solutions/task01.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*Задача 1: Напишете програма, която чете от клавиатурата две числа 
-и пресмята и извежда на конзолата тяхното средно аритметично.*/
-
 int main() {
 	double firstNumber = 0;
 	double secondNumber = 0;
