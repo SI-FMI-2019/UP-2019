c6cee1b4dbefe2cb24e1836a1f9c7f0f95272818
MimiFMI <maria.paskova@abv.bg>
2019-11-12 10:52:33 +0200

    Add solutions from lab 6 group 5

---
+++ group5/2019.11.12_6_Functions/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+//int sumDel(int a){
+//    int sum=0;
+//    for(int i=1;i<=a;i++){
+//        if(a%i==0) sum+=i;
+//    }
+//    return sum;
+//}
+//void isPrime(int a)
+//{
+//    bool istrue;
+//    for(int i=2;i<a;i++)
+//
+//    {
+//        if (a%i==0) istrue=1;
+//    }
+//    if(istrue) cout<<"The number isn't prime";
+//    else cout<<"The number is prime";
+//}
+//
+//int countDivisors(int n)
+//{
+//    int count = 0;
+//
+//    for (int i = 2; i < n; i++)
+//    {
+//        if (n%i ==0)
+//
+//            count ++;
+//    }
+//    return count;
+//
+//}
+
+
+//int main()
+//{
+//
+//
+////    printf("The count of divisor: ");
+////    int countDiv = countDivisors(20);
+////    cout<<countDiv;
+////    isPrime(countDiv);
+//    return 0;
+//}
+
+
+//Fibb
+int Fibonachi(double number)
+{
+    int a0= 0, a1=1;
+    for (int previousNum, int sum=0;sum<number; sum)
+    {
+
+        int sum= a0+a1;
+        sum=previousNum
+
+    }
+    int a0= 0, a1=1;
+
+    int N_number_sum=
+}
+int main()
+{
+
+}
